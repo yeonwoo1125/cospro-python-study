2941604b08b7517c2734ef3948a4538e011499d6
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-12-02 13:40:04 +0900

    [ #4 ] 등장 하는 가장 많은 수와 적은 수 구하기(yeonwoo)

---
+++ yeonwoo/answer/등장하는_가장_많은_수와_적은 수_구하기.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+def func_a(arr):
+	counter = [0 for _ in range(1001)]
+	for x in arr:
+		counter[x] += 1
+	return counter
+
+def func_b(arr):
+	ret = 0
+	for x in arr:
+		if ret < x:
+			ret = x
+	return ret
+
+def func_c(arr):
+	INF = 1001
+	ret = INF
+	for x in arr:
+		if x != 0 and ret > x:
+			ret = x
+	return ret
+
+
+def solution(arr):
+	counter = func_a(arr)
+	max_cnt = func_b(counter)
+	min_cnt = func_c(counter)
+	return max_cnt // min_cnt
+
+
+if __name__ == '__main__':
+  arr = [1, 2, 3, 3, 1, 3, 3, 2, 3, 2]
+  ret = solution(arr)
+
+  print("solution 함수의 반환 값은", ret, "입니다.")
