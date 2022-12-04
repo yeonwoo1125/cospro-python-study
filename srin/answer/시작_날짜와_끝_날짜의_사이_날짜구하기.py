afa03c01ff33f2bdfd504a13d02fa445bd003915
Serin Kim <68180798+SRin23@users.noreply.github.com>
2022-12-04 22:16:42 +0900

    [#3] 시작 날짜와 끝 날짜의 사이 날짜구하기(srin)

---
+++ srin/answer/시작_날짜와_끝_날짜의_사이_날짜구하기.py
@@ -0,0 +1,20 @@
+def func_a(month, day):
+	month_list = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+	total = 0;
+	for i in range(month-1):
+		total += month_list[i]
+	total += day
+	return total - 1
+
+def solution(start_month, start_day, end_month, end_day):
+	start_total = func_a(start_month, start_day)
+	end_total = func_a(end_month, end_day)
+	return end_total - start_total
+
+start_month = 1
+start_day = 2
+end_month = 2
+end_day = 2
+ret = solution(start_month, start_day, end_month, end_day)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
