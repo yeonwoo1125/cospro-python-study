2696debb15a46e6d589ad51562630ced7b5f85a9
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-12-04 21:25:25 +0900

    [ #10 ] 평균 이하의 개수 구하기(yeonwoo)

---
+++ yeonwoo/answer/평균_이하의_개수_구하기.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# UTF-8 encoding when using korean
+
+def solution(data):
+	total = sum(data)
+	average = total / len(data) 
+	cnt = 0
+	for d in data:
+		if d <= average:
+			cnt += 1
+	return cnt
+
+if __name__ == '__main__':
+  data1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+  ret1 = solution(data1)
+
+  print("solution 함수의 반환 값은", ret1, "입니다.")
+
+  data2 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 10]
+  ret2 = solution(data2)
+
+  print("solution 함수의 반환 값은", ret2, "입니다.")
