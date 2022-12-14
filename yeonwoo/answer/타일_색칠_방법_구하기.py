4dac8ec04cfc58d732c9a5f2940c42791d184c1b
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-12-14 11:13:23 +0900

    [ #26 ] 타일 색칠 방법 구하기(yeonwoo)

---
+++ yeonwoo/answer/타일_색칠_방법_구하기.py
@@ -0,0 +1,20 @@
+def solution(tile_length):
+	answer = ''
+	com = 'RRRGGB'
+	if tile_length%6 == 1 or tile_length%6 == 2 or tile_length%6 == 4:
+		answer = '-1'
+	else:
+		for i in range(tile_length):
+			answer += com[i % 6]
+	return answer
+
+if __name__ == '__main__':
+  tile_length1 = 11
+  ret1 = solution(tile_length1)
+
+  print("solution 함수의 반환 값은", ret1, "입니다.")
+
+  tile_length2 = 16
+  ret2 = solution(tile_length2)
+
+  print("solution 함수의 반환 값은", ret2, "입니다.")
