f26100926eddad8612017db554ed385fc21c4595
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-12-16 11:01:34 +0900

    [ #46 ] 이제 수학 시험에 모든 걸 건다.(yeonwoo)

---
+++ yeonwoo/answer/이제_수학_시험에_모든_걸_건다.py
@@ -0,0 +1,15 @@
+def solution(korean, english):
+	answer = 0
+	math = 210 - (korean + english)
+	if math > 100:
+		answer = -1
+	else:
+		answer = math
+	return answer
+ 
+if __name__ == '__main__':
+  korean = 70
+  english = 60
+  ret = solution(korean, english)
+
+  print("solution 함수의 반환 값은", ret, "입니다.")
