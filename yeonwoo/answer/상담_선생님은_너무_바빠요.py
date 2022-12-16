3ce7bcd2f63547c909e6897db458d1fe07f392b2
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-12-16 09:55:26 +0900

    [ #31 ] 상담 선생님은 너무 바빠요(yeonwoo)

---
+++ yeonwoo/answer/상담_선생님은_너무_바빠요.py
@@ -0,0 +1,12 @@
+def solution(schedule):
+	answer = []
+	for idx, i in enumerate(schedule):
+		if i == 'X':
+			answer.append(idx+1)
+	return answer
+
+if __name__ == '__main__':
+  schedule = ["O", "X", "X", "O", "O", "O", "X", "O", "X", "X"]
+  ret = solution(schedule)
+
+  print("solution 함수의 반환 값은", ret, "입니다.")
