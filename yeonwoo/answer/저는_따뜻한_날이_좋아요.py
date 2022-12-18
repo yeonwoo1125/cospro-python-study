39944899307ebe00cc29c036fd8f13d93bc5e1e1
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-12-18 23:27:31 +0900

    [ #51 ] 저는 따뜻한 날이 좋아요.(yeonwoo)

---
+++ yeonwoo/answer/저는_따뜻한_날이_좋아요.py
@@ -0,0 +1,14 @@
+def solution(temperature, A, B):
+	answer = 0
+	for i in range(0, len(temperature)):
+		if temperature[i] >= temperature[A] and temperature[i] < temperature[B]:
+			answer += 1
+	return answer
+
+if __name__ == '__main__':
+  temperature = [3, 2, 1, 5, 4, 3, 3, 2]
+  A = 1
+  B = 6
+  ret = solution(temperature, A, B)
+
+  print("solution 함수의 반환 값은", ret, "입니다.")
