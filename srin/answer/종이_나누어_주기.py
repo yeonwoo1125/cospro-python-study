80c087f124b65b63298cea9d26e3927bb220271b
Serin Kim <68180798+SRin23@users.noreply.github.com>
2022-12-18 21:23:40 +0900

    [#52] 종이 나누어 주기(srin)

---
+++ srin/answer/종이_나누어_주기.py
@@ -0,0 +1,19 @@
+def solution(papers, K):
+	length = len(papers)
+	for i, paper in enumerate(papers):
+		K -= paper
+		if K < 0:
+			length = i-1
+	return length
+
+papers1 = [2, 4, 2, 3, 1]
+K1 = 10
+ret1 = solution(papers1, K1)
+
+print("solution 함수의 반환 값은", ret1, "입니다.")
+
+papers2 = [2, 4, 2, 3, 1]
+K2 = 14
+ret2 = solution(papers2, K2)
+
+print("solution 함수의 반환 값은", ret2, "입니다.")
