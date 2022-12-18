0b2f12fdb14ede7f1e57220b7be295e948ad92c2
Serin Kim <68180798+SRin23@users.noreply.github.com>
2022-12-18 20:51:34 +0900

    [#41] 사다리 게임의 승리자를 구해주세요!(srin)

---
+++ srin/answer/사다리_게임의_승리자를_구해주세요!.py
@@ -0,0 +1,15 @@
+def solution(ladders, win):
+	answer = 0
+	player = [1, 2, 3, 4, 5, 6]
+	for e in ladders:
+		temp = player[e[0]-1]
+		player[e[0] -1] = player[e[1]-1]
+		player[e[1]-1] = temp
+	answer = player[win-1]
+	return answer
+
+ladders = [[1, 2], [3, 4], [2, 3], [4, 5], [5, 6]]
+win = 3
+ret = solution(ladders, win)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
