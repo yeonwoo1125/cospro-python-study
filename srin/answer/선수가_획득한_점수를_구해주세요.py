6d3bb8fdffd782bd90efb8aa846472f9e1284698
Serin Kim <68180798+SRin23@users.noreply.github.com>
2022-12-18 20:53:42 +0900

    [#44] 선수가 획득한 점수를 구해주세요.(srin)

---
+++ srin/answer/선수가_획득한_점수를_구해주세요.py
@@ -0,0 +1,22 @@
+def solution(taekwondo, running, shooting):
+	answer = 0
+	if taekwondo >= 25:
+		answer += 250
+	else:
+		answer += taekwondo * 8
+	answer += 250 + (60 - running) * 5
+	count = 0
+	for s in shooting:
+		answer += s
+		if s == 10:
+			count += 1
+	if count >= 7:
+		answer += 100
+	return answer
+
+taekwondo = 27
+running = 63
+shooting = [9, 10, 8, 10, 10, 10, 7, 10, 10, 10]
+ret = solution(taekwondo, running, shooting)
+
+print("solution 함수의 반환 값은", ret, "입니다.") 
