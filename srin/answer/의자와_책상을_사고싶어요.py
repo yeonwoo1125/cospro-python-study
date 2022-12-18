deb3536343950eef1512880ab454d6803c5d319d
Serin Kim <68180798+SRin23@users.noreply.github.com>
2022-12-18 21:26:17 +0900

    [#57] 의자와 책상을 사고싶어요.(srin)

---
+++ srin/answer/의자와_책상을_사고싶어요.py
@@ -0,0 +1,22 @@
+def solution(money, chairs, desks):
+	answer = 0
+	for chair in chairs:
+		for desk in desks:
+			price = (desk + chair)
+			if answer < price and price <= money:
+				answer = price
+	return answer
+  
+money1 = 7
+chairs1 = [2, 5]
+desks1 = [4, 3, 5]
+ret1 = solution(money1, chairs1, desks1)
+
+print("solution 함수의 반환 값은", ret1, "입니다.")
+
+money2 = 7
+chairs2 = [3]
+desks2 = [5]
+ret2 = solution(money2, chairs2, desks2)
+
+print("solution 함수의 반환 값은", ret2, "입니다.")
