fedbd2169621d0019be4b3afa3aee4d6d3c60b23
Serin Kim <68180798+SRin23@users.noreply.github.com>
2022-12-18 20:53:03 +0900

    [#43] 벌금 구하기!(srin)

---
+++ srin/answer/벌금_구하기!.py
@@ -0,0 +1,16 @@
+def solution(speed, cars):
+	answer = 0
+	for x in cars:
+		if x >= speed * 11 / 10 and x < speed * 12 / 10:
+			answer += 3
+		elif x >= speed * 12 / 10 and x < speed * 13 / 10:
+			answer += 5
+		elif x >= speed * 13 / 10:
+			answer += 7
+	return answer
+  
+speed = 100
+cars = [110, 98, 125, 148, 120, 112, 89]
+ret = solution(speed, cars)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
