6fa72216e215ad6661dcde44c775ee63fe3ab2e5
Serin Kim <68180798+SRin23@users.noreply.github.com>
2022-12-18 19:11:11 +0900

    [#33] 획득 점수 구하기. 도박은 안되요.(srin)

---
+++ srin/answer/획득_점수_구하기._도박은_안되요.py
@@ -0,0 +1,36 @@
+def func_a(bundle, start):
+	return bundle[start::2]
+
+def func_b(score1, score2):
+	if score1 > score2:
+		return [1, score1]
+	elif score2 > score1:
+		return [2, score2]
+	else:
+		return [0, score1]
+
+def func_c(bundle):
+	answer = 0
+	score_per_cards = {
+		'a': 1,
+		'b': 2,
+		'c': 3,
+		'd': 4,
+		'e': 5
+	}
+	for card in bundle:
+		answer += score_per_cards[card]
+	return answer
+  
+def solution(n, bundle):
+a_cards = func_a(bundle, 1)[:n]
+b_cards = func_a(bundle, 2)[:n]
+a_score = func_c(a_cards)
+b_score = func_c(b_cards)
+return func_b(a_score, b_score)
+
+n = 4
+bundle = "cacdbdedccbb"
+ret = solution(n, bundle)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
