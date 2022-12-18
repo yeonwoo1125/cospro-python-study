4efe8648cf1071ac35a2ce7e90bf87873c5c4a30
Serin Kim <68180798+SRin23@users.noreply.github.com>
2022-12-18 19:13:20 +0900

    [#34] 조교의 수 구하기(srin)

---
+++ srin/answer/조교의_수_구하기.py
@@ -0,0 +1,13 @@
+def solution(classes, m):
+	answer = 0
+	for students in classes:
+		answer += students // m
+		if students % m != 0:
+			answer += 1
+	return answer
+
+classes = [80, 45, 33, 20]
+m = 30
+ret = solution(classes, m)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
