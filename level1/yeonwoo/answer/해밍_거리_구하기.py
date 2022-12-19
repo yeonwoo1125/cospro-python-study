cb6a1db7978601422589715d854f930588ca07a2
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-12-19 22:15:59 +0900

    [ #62 ] 해밍 거리 구하기(yeonwoo)

---
+++ level1/yeonwoo/answer/해밍_거리_구하기.py
@@ -0,0 +1,24 @@
+def func_a(string, length):
+	padZero = ""
+	padSize = len(string)
+	for i in range(padSize):
+		padZero += "0"
+	return padZero + string
+
+def solution(binaryA, binaryB):
+	max_length = max(len(binaryA), len(binaryB))
+	binaryA = func_a(binaryA, max_length)
+	binaryB = func_a(binaryB, max_length)
+
+	hamming_distance = 0
+	for i in range(max_length):
+		if not binaryA[i] == binaryB[i]:
+			hamming_distance += 1
+	return hamming_distance
+
+
+binaryA = "10010"
+binaryB = "110"
+ret = solution(binaryA, binaryB)
+
+print("solution 함수의 반환 값은", ret, "입니다.")
