a7ecea6f1f2673a3c55ea077bef8af22245f96b5
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-12-25 22:39:55 +0900

    [ #69 ] 계단 게임(yeonwoo)

---
+++ level1/yeonwoo/answer/계단_게임.py
@@ -0,0 +1,24 @@
+def func(record):
+	if record == 0:
+		return 1
+	elif record == 1:
+		return 2
+	return 0
+
+def solution(recordA, recordB):
+	cnt = 0
+	for i in range(len(recordA)):
+			if recordA[i] == recordB[i]:
+					continue
+			elif recordA[i] == func(recordB[i]):
+					cnt = cnt + 3
+			elif recordB[i] > recordA[i]:
+					cnt = cnt - 1
+	return cnt
+
+recordA = [2,0,0,0,0,0,1,1,0,0]
+recordB = [0,0,0,0,2,2,0,2,2,2]
+ret = solution(recordA, recordB)
+
+
+print("solution 함수의 반환 값은", ret, "입니다.")
